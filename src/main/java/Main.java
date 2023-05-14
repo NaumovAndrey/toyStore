0baba70cf1047f7b1e47950ed72b0843a353ab8a
andrey <naumov.a.n@icloud.com>
2023-05-14 14:15:27 +0700

    много чего понять еще немогу, вот простой вариант

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,66 +1,54 @@
-import org.example.menu.Menu;
-import org.example.menu.MenuItem;
-import org.example.menu.SubMenu;
+import org.example.toyStore.Toy;
 import org.example.toyStore.ToyStore;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        ToyStore store = new ToyStore();
-        store.loadFromFile();
-
-        menu.addItem(new MenuItem(1, "Создать розыгрыш",
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("метод чтение из файла, метод логики выбора призовой игрушки, запись в файл массива призовых игрушек и статус выдачи");
-                    }
-                }));
-        menu.addItem(new MenuItem(2, "Итория розыгрышей",
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("метод чтение из файла, метод статуса получения призовой игрушки ");
-
-                    }
-                }));
-
-        menu.addItem(new MenuItem(3, "Опции",
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.out.println("Метод показывает все игрушки, создаёт новую, изменяет старую, и удаляет");
-
-                        SubMenu subMenu = new SubMenu();
-                        subMenu.addItem(new MenuItem(1, "Сздать",
-                                new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        System.out.println("Метод создания нвой игрушки");
-                                    }
-                                }));
-                    }
-                }));
-
-
-        menu.addItem(new MenuItem(4, "Выйти из программы",
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        System.exit(0);
-                    }
-                }));
-
-        while (true) {
-            menu.display();
+        public static void main(String[] args) {
+            ToyStore store = new ToyStore();
+            store.loadFromFile();
 
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Введите номер команды: ");
-            int id = scanner.nextInt();
-            menu.runCommand(id);
-            System.out.println();
+            while (true) {
+                System.out.println("1. Add toy");
+                System.out.println("2. Change toy weight");
+                System.out.println("3. Play");
+                System.out.println("4. Exit");
+                System.out.print("> ");
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        System.out.print("Enter toy name: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Enter toy quantity: ");
+                        int quantity = scanner.nextInt();
+                        System.out.print("Enter toy weight: ");
+                        int weight = scanner.nextInt();
+                        store.addToy(new Toy(name, quantity, weight));
+                        store.saveToFile();
+                        break;
+                    case 2:
+                        System.out.print("Enter toy id: ");
+                        int id = scanner.nextInt();
+                        System.out.print("Enter toy weight: ");
+                        weight = scanner.nextInt();
+                        store.changeToyWeight(id, weight);
+                        store.saveToFile();
+                        break;
+                    case 3:
+                        Toy toy = store.play();
+                        if (toy != null) {
+                            System.out.println("You won a " + toy.getName() + "!");
+                        } else {
+                            System.out.println("Sorry, no toy available.");
+                        }
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Invalid choice.");
+                }
+            }
         }
     }
-}
--- src/main/java/org/example/toyStore/ToyStore.java
+++ src/main/java/org/example/toyStore/ToyStore.java
@@ -65,52 +65,6 @@ public class ToyStore {
         return null;
     }
 
-//    public static void main(String[] args) {
-//        ToyStore store = new ToyStore();
-//        store.loadFromFile();
-//
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            System.out.println("1. Add toy");
-//            System.out.println("2. Change toy weight");
-//            System.out.println("3. Play");
-//            System.out.println("4. Exit");
-//            System.out.print("> ");
-//            int choice = scanner.nextInt();
-//            scanner.nextLine();
-//            switch (choice) {
-//                case 1:
-//                    System.out.print("Enter toy name: ");
-//                    String name = scanner.nextLine();
-//                    System.out.print("Enter toy quantity: ");
-//                    int quantity = scanner.nextInt();
-//                    System.out.print("Enter toy weight: ");
-//                    int weight = scanner.nextInt();
-//                    store.addToy(new Toy(name, quantity, weight));
-//                    store.saveToFile();
-//                    break;
-//                case 2:
-//                    System.out.print("Enter toy id: ");
-//                    int id = scanner.nextInt();
-//                    System.out.print("Enter toy weight: ");
-//                    weight = scanner.nextInt();
-//                    store.changeToyWeight(id, weight);
-//                    store.saveToFile();
-//                    break;
-//                case 3:
-//                    Toy toy = store.play();
-//                    if (toy != null) {
-//                        System.out.println("You won a " + toy.getName() + "!");
-//                    } else {
-//                        System.out.println("Sorry, no toy available.");
-//                    }
-//                    break;
-//                case 4:
-//                    return;
-//                default:
-//                    System.out.println("Invalid choice.");
-//            }
-//        }
-//    }
+
 }
 
